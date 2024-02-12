87ef2601d72ffaed2c098191e2cb9ef0152e475a
Robin Gauthier <rgauthier@quatrecentquatre.com>
2024-02-12 14:17:33 -0500

    remove php8 match comments

--- src/helpers/DummyDataHelpers.php
+++ src/helpers/DummyDataHelpers.php
@@ -14,40 +14,6 @@ class DummyDataHelpers extends Component
         $faker = Faker\Factory::create();
         $assetsService = new DummyAssetsService();
 
-        /*
-            PHP 8+
-        $return_value = match ($type) {
-            'address' => $faker->streetAddress(),
-            'userAgent' => $faker->userAgent(),
-            'ip' => $faker->ipv4(),
-            'url' => $faker->url(),
-            'userName' => $faker->userName(),
-            'name' => $faker->name(),
-            'email' => $faker->email(),
-            'firstName' => $faker->firstName(),
-            'lastName' => $faker->lastName(),
-            'stateAbbr' => $faker->stateAbbr(),
-            'streetName' => $faker->streetName(),
-            'streetAddress' => $faker->streetAddress(),
-            'secondaryAddress' => $faker->secondaryAddress(),
-            'postcode' => $faker->postcode(),
-            'city' => $faker->city(),
-            'latitude' => $faker->latitude($min = -90, $max = 90),
-            'longitude' => $faker->longitude($min = -180, $max = 180),
-            'phoneNumber' => $faker->phoneNumber(),
-            'date' => $faker->date('Y-m-d H:i:s'),
-            'text' => $faker->text(100),
-
-            'image' => $assetsService->getAsset('jpg'),
-            'word' => $assetsService->getAsset('doc'),
-            'txt' => $assetsService->getAsset('txt'),
-            'pdf' => $assetsService->getAsset('pdf'),
-            'excel' => $assetsService->getAsset('xlsx'),
-            'compressed' => $assetsService->getAsset('zip'),
-
-            default => $default ?? '',
-        };
-        */
         switch ($type) {
             case 'address' :
                 $return_value = $faker->streetAddress();
